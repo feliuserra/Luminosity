41ce7b6ac7ae765189829071f169a02f1e489e5d
Westermann <westermann93@googlemail.com>
2017-06-08 00:41:23 +0200

    Fix returns script

--- src/features/create_returns.py
+++ src/features/create_returns.py
@@ -1,16 +1,15 @@
 import glob
 import numpy as np
-from scipy import sparse as ss
 
 
 files = glob.glob(
     'data/raw/Version_4_DMSP-OLS_Nighttime_Lights_Time_Series/*')
 for i in range(1, len(files)):
     print(files[i])
-    A = ss.csr_matrix(np.load(files[i-1])['arr_0'])
-    B = ss.csr_matrix(np.load(files[i])['arr_0'])
-    C = B - A
-    np.savez_compressed(files[i]
+    A = np.load(files[i-1])['arr_0']
+    B = np.load(files[i])['arr_0']
+    C = np.diff(np.stack([B, A]).astype(float), axis=0)  # convert because it's
+    np.savez_compressed(files[i]                         # `uint8` type
                         .replace('raw', 'interim')
                         .replace('Series', 'Returns'),
-                        C.todense())
+                        C[0])
