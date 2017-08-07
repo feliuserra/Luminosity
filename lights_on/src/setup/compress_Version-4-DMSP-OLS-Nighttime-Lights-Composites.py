7ac06729197f3fdcb8472e9fabb5342e1ca62caa
Westermann <westermann93@googlemail.com>
2017-08-08 00:16:51 +0200

    Attempt to solve memory issue V

--- lights_on/src/setup/compress_Version-4-DMSP-OLS-Nighttime-Lights-Composites.py
+++ lights_on/src/setup/compress_Version-4-DMSP-OLS-Nighttime-Lights-Composites.py
@@ -9,9 +9,9 @@ def create_compressed_numpy_arrays(filelist):
     for fn in filelist:
         year = re.search('(?<=F\d{2})\d{4}', fn).group(0)
         with TiffFile(str(wd) + '/data/downloads/Version_4_DMSP-OLS_Nighttime_Lights_Time_Series/' + fn) as tif:
-            with tif.asarray() as arr:
-                np.savez_compressed(str(wd) + '/data/nightlights/year/' + str(year), arr)
-                del arr
+            arr = tif.asarray()
+            np.savez_compressed(str(wd) + '/data/nightlights/year/' + str(year), arr)
+            del arr
 
         gc.collect()
 
