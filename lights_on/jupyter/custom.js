ddcbd30a01742ccc7b9e0b13aa08c1dcbde2dba0
Westermann <westermann93@googlemail.com>
2017-08-10 15:41:28 +0200

    Add custom js

---
+++ lights_on/jupyter/custom.js
@@ -0,0 +1,18 @@
+[
+  'Dockerfile',
+  'README.md',
+  'start-singleuser.sh',
+  'start.sh',
+  'requirements.txt',
+  'src',
+  'jupyter'
+].forEach(function(s){
+  $('.list_item:contains("' + s + '")').hide()
+})
+
+$('#notebook_toolbar').clone().insertBefore('#notebook_toolbar').removeClass('list_toolbar').empty()
+  .append(
+      '<h2>Welcome to the Nightlight Exploratory</h2>' +
+      '<p style="font-size: 14px; font-weight: bold;">Access and explore the data through the Jupyter notebooks in the notebooks folder. If this is your first time using jupyter notebooks and this package, then you should check out the <a href="/notebooks/notebooks/getting_started.ipynb">Getting Started</a> notebook.</p>'
+      )
+
